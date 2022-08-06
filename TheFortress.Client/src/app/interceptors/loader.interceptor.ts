c617fe81c4fa796a530f72d5a2dff855a8b1dceb
Mikel <mikelmayorga08@gmail.com>
2022-08-05 18:02:52 -0700

    load spinner interceptor can handle multiple outgoing requests

--- TheFortress.Client/src/app/interceptors/loader.interceptor.ts
+++ TheFortress.Client/src/app/interceptors/loader.interceptor.ts
@@ -3,7 +3,8 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators'
@@ -11,14 +12,62 @@ import { SpinnerOverlayService } from '../services/spinner-overlay.service';
 
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
+  private requests: HttpRequest<any>[] = [];
 
-  constructor(public loaderService: SpinnerOverlayService) {}
+  constructor(public loaderService: SpinnerOverlayService) { }
+
+  removeRequest(req: HttpRequest<any>) {
+    const i = this.requests.indexOf(req);
+    if (i >= 0) {
+      this.requests.splice(i, 1);
+    }
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // const subscription = this.loaderService.spinner$.subscribe();
-    this.loaderService.show();
-    return next.handle(request).pipe(
-      finalize(() => this.loaderService.hide())
-    );
+
+    this.requests.push(request);
+    if (this.requests.length < 1) {
+      this.loaderService.show();
+    }
+    return new Observable(observer => {
+      const subscription = next.handle(request)
+        .subscribe(
+          event => {
+            console.log(event); // I CAN SEE THE LOADED EVENT HERE
+            if (event instanceof HttpResponse) {
+              this.removeRequest(request);
+            }
+            observer.next(event);
+          },
+          err => {
+            // this.alertService.setAlert('error', err.message);
+            this.removeRequest(request);
+            observer.error(err);
+            this.loaderService.hide();
+          },
+          () => {
+            this.removeRequest(request);
+            observer.complete();
+            this.loaderService.hide();
+          });
+      // remove request from queue when cancelled
+      return () => {
+        this.removeRequest(request);
+        subscription.unsubscribe();
+        this.loaderService.hide();
+
+      };
+    });
+    // return next.handle(request).pipe(request => {
+    //   request.pipe((event) => {
+    //     if (event instanceof HttpResponse) {
+    //       this.removeRequest(request);
+    //     }
+
+    //   })
+    // }
+    //   finalize(() => this.loaderService.hide())
+    // );
   }
 }
