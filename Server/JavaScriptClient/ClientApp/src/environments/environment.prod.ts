0732040ff8bdf76d2b3a18d524378ba0bb2086cc
Mikel <mikelmayorga08@gmail.com>
2022-08-30 22:30:05 -0700

    delete JS BFF

--- Server/JavaScriptClient/ClientApp/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  clientId: '9def66a9-e8c2-4d7a-aaab-1f60a37c2178',
-  cloudInstance: 'https://login.microsoftonline.com',
-  directoryId: 'd998c8a0-76ce-4b43-8c27-7d17ac97e4d0',
-  redirectUri: '/',
-};
