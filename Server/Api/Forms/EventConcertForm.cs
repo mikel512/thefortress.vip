b10415146903749b5faf375660ff43dc9cb46b0c
Mikel <mikelmayorga08@gmail.com>
2022-09-04 00:34:00 -0700

    Start forms code generator

---
+++ Server/Api/Forms/EventConcertForm.cs
@@ -0,0 +1,19 @@
+﻿
+using Api.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Forms
+{
+    [FormGenerate]
+    public class EventConcert
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public string? FlyerUrl { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public string? PriceAmount { get; set; }
+        public string? Time { get; set; }
+    }
+}
