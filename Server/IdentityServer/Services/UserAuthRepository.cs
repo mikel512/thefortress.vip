fb6e0b66973500159273dbc92a1c7aa560bc2adc
Mikel <mikelmayorga08@gmail.com>
2022-11-10 17:26:50 -0800

    Fix JWT options

--- Server/IdentityServer/Services/UserAuthRepository.cs
+++ Server/IdentityServer/Services/UserAuthRepository.cs
@@ -47,16 +47,16 @@ namespace IdentityServer.Services
             code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
 
             String callbackUrl = null;
-            if(_env == "Development")
+            if (_env == "Development")
             {
                 callbackUrl = $"https://localhost:4200/auth/confirm-email/{user.Id}/{code}";
             }
             else
             {
-                callbackUrl = $"https://thefortress.vip/auth/confirm-email/{user.Id}/{code}"; 
+                callbackUrl = $"https://thefortress.vip/auth/confirm-email/{user.Id}/{code}";
             }
             await _emailService.SendMailAsync("admin@thefortress.vip", "Admin", InputModel.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode( callbackUrl)}'>clicking here</a>.");
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
             return result;
         }
@@ -65,7 +65,7 @@ namespace IdentityServer.Services
             await EnsureSeedRoles();
             // first try by username
             _user = await _userManager.FindByNameAsync(loginDto.Username);
-            if(_user == null)
+            if (_user == null)
             {
                 // if username not found try email
                 _user = await _userManager.FindByEmailAsync(loginDto.Username);
@@ -103,10 +103,23 @@ namespace IdentityServer.Services
         private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
         {
             var jwtSettings = _config.GetSection("JwtConfig");
+
+            string iss = "";
+            string aud = "";
+            if (_env == "Development")
+            {
+                iss = jwtSettings["ValidIssuerDEV"];
+                aud = jwtSettings["ValidAudienceDEV"]; 
+            }
+            else
+            {
+                iss = jwtSettings["ValidIssuerPROD"];
+                aud = jwtSettings["ValidAudiencePROD"]; 
+            }
             var tokenOptions = new JwtSecurityToken
             (
-            issuer: jwtSettings["ValidIssuerDEV"],
-            audience: jwtSettings["ValidAudienceDEV"],
+            issuer: iss,
+            audience: aud,
             claims: claims,
             expires: DateTime.Now.AddHours(Convert.ToDouble(jwtSettings["TimeTilExp"])),
             signingCredentials: signingCredentials
@@ -141,13 +154,13 @@ namespace IdentityServer.Services
         }
 
         private async Task EnsureSeedRoles()
-        { 
+        {
             if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
                 await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
             if (!await _roleManager.RoleExistsAsync(UserRoles.User))
-                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User)); 
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
             if (!await _roleManager.RoleExistsAsync(UserRoles.Artist))
-                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Artist)); 
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Artist));
         }
     }
 }
