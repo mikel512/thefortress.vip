951fb7d65e7e73037a7615ddba7a4a187ff7eb4e
Mikel <mikelmayorga08@gmail.com>
2022-11-15 23:23:57 -0800

    add edit button to venues

--- TheFortress.Client/src/app/pages/admin/venues-list.component.ts
+++ TheFortress.Client/src/app/pages/admin/venues-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { VenueFormModelFormComponent } from '@forms/venue-form-model-form.component';
 import { Venue } from '@models/venue';
+import { VenueFormModel } from '@models/venue-form-model';
 import { VenueService } from '@services/venue.service';
+import { AppModalComponent } from '../shared/modal.component';
 
 @Component({
     selector: 'app-venues-list',
@@ -9,6 +12,10 @@ import { VenueService } from '@services/venue.service';
 
 export class VenuesListComponent implements OnInit {
     public venues: Venue[] = [];
+    public editVenue: Venue;
+
+    @ViewChild(VenueFormModelFormComponent) form: VenueFormModelFormComponent;
+    @ViewChild(AppModalComponent) modal: AppModalComponent;
 
     constructor(private venueData: VenueService) { }
 
@@ -18,5 +25,30 @@ export class VenuesListComponent implements OnInit {
                 this.venues = x;
             }
         })
+    }
+
+    edit(venue: Venue) {
+        this.editVenue = venue;
+        this.form.inputForm.setValue({
+            venueName: venue.venueName,
+            address: venue.address,
+            picture: venue.picture,
+            location: venue.location,
+            description: venue.description,
+            ticketsLink: venue.ticketsLink,
+            menuLink: venue.menuLink,
+            hours: venue.hours
+        })
+        this.modal.show();
+    }
+
+    update(updateVenue: VenueFormModel){
+        let update = new Venue({
+            venueName: updateVenue.venueName,
+            address: updateVenue.address,
+            location: updateVenue.location,
+
+        });
+
     }
 }
