865748c584e508cd4d2761c6cca8439c0910bb6b
Mikel <mikelmayorga08@gmail.com>
2023-01-24 22:09:08 -0800

    update application user

---
+++ Server/IdentityServer/Migrations/20230124034300_addMailingListUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityServer.Migrations
+{
+    public partial class addMailingListUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MailingListEnabled",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MailingListEnabled",
+                table: "AspNetUsers");
+        }
+    }
+}
