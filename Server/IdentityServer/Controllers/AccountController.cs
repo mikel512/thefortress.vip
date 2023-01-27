44493fedd28fc56481c434409df97727e5b6a543
Mikel <mikelmayorga08@gmail.com>
2023-01-27 14:38:03 -0800

    add change password endpoints

--- Server/IdentityServer/Controllers/AccountController.cs
+++ Server/IdentityServer/Controllers/AccountController.cs
@@ -34,17 +34,22 @@ namespace IdentityServer.Controllers
                 JwtSecurityToken token = new JwtSecurityTokenHandler().ReadJwtToken(req);
                 string userId = token.Claims.Where(x => x.Type == ClaimTypes.Sid).FirstOrDefault().Value;
 
-                if(userId == null)
+                if (userId == null)
                 {
                     return new StatusCodeResult(StatusCodes.Status403Forbidden);
                 }
 
                 ApplicationUser user = await _userManager.FindByIdAsync(userId);
+                if (user == null)
+                {
+                    return new StatusCodeResult(StatusCodes.Status404NotFound);
+                }
+
                 user.UserName = appUserDto.UserName;
                 user.Email = appUserDto.Email;
                 user.MailingListEnabled = appUserDto.MailingListEnabled;
 
-                await _userManager.UpdateAsync(user); 
+                await _userManager.UpdateAsync(user);
 
                 // TODO send email to new email if it changed
 
@@ -67,12 +72,16 @@ namespace IdentityServer.Controllers
                 JwtSecurityToken token = new JwtSecurityTokenHandler().ReadJwtToken(req);
                 string userId = token.Claims.Where(x => x.Type == ClaimTypes.Sid).FirstOrDefault().Value;
 
-                if(userId == null)
+                if (userId == null)
                 {
                     return new StatusCodeResult(StatusCodes.Status403Forbidden);
                 }
 
                 ApplicationUser user = await _userManager.FindByIdAsync(userId);
+                if (user == null)
+                {
+                    return new StatusCodeResult(StatusCodes.Status404NotFound);
+                }
 
                 AppUserDto result = _mapper.Map<AppUserDto>(user);
 
@@ -84,5 +93,39 @@ namespace IdentityServer.Controllers
             }
         }
 
+        [HttpPut("[action]")]
+        [Authorize]
+        public async Task<IActionResult> ChangePassword([FromBody] AppUserDto appUser)
+        {
+            try
+            {
+                var req = Request.Headers.Authorization[0].Replace("Bearer ", "");
+                JwtSecurityToken token = new JwtSecurityTokenHandler().ReadJwtToken(req);
+
+                string userId = token.Claims.Where(x => x.Type == ClaimTypes.Sid).FirstOrDefault().Value;
+                if (userId == null)
+                {
+                    return new StatusCodeResult(StatusCodes.Status403Forbidden);
+                }
+
+                ApplicationUser user = await _userManager.FindByIdAsync(userId); 
+                if (user == null)
+                {
+                    return new StatusCodeResult(StatusCodes.Status404NotFound);
+                }
+
+                var changePasswordResult = await _userManager.ChangePasswordAsync(user, appUser.OldPassword, appUser.Password);
+                if(!changePasswordResult.Succeeded)
+                {
+                    return new StatusCodeResult(StatusCodes.Status400BadRequest); 
+                }
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return new StatusCodeResult(500);
+            }
+        }
     }
 }
--- Server/IdentityServer/Models/AppUserDto.cs
+++ Server/IdentityServer/Models/AppUserDto.cs
@@ -4,6 +4,8 @@
     {
         public string UserName { get; set; }
         public string Email { get; set; }
+        public string OldPassword { get; set; }
+        public string Password { get; set; }
         public bool MailingListEnabled { get; set; }
     }
 }
